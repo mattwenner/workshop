f9795fa5b307b8d142ad20e00c32ece811cc5bcc
Matthew Asao Wenner <mwenner@usc.edu>
2020-05-08 09:23:46 -0700

    Cast product type from JSON mock

--- backend/src/data-access/index.ts
+++ backend/src/data-access/index.ts
@@ -2,21 +2,13 @@ import { Product } from "./models/product";
 import mockProducts from "./mocks/products.json";
 import { Review } from "./models/review";
 import mockReviews from "./mocks/reviews.json";
-import { Category } from "../generated/types";
-
-function mapProduct(product: any): Product {
-  const categories = product.categories.map(
-    (category: string) => Category[category as keyof typeof Category]
-  );
-  return { categories, ...product };
-}
 
 export async function getProducts(): Promise<Product[]> {
-  return mockProducts.map(mapProduct);
+  return mockProducts as Product[];
 }
 
 export async function getProductById(id: string): Promise<Product | null> {
-  return mockProducts.map(mapProduct).find((p: Product) => p.id === id) ?? null;
+  return (mockProducts as Product[]).find((p: Product) => p.id === id) ?? null;
 }
 
 export async function getReviewsForProduct(
